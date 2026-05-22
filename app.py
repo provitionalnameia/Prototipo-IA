ab66aa3337f24e391aa1fdf8192ae1d1f41dc1ed
FutureIA Systems <futureia.systems@gmail.com>
2026-05-21 21:59:05 -0600

    final fix

--- app.py
+++ app.py
@@ -1,10 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>FUNCIONA</h1>"
+    return render_template("index.html")
+
+@app.route("/atencion")
+def atencion():
+    return """
+    <h1 style='color:white;
+    font-family:Arial;
+    text-align:center;
+    margin-top:100px;
+    background:black;'>
+    Página de atención al cliente próximamente
+    </h1>
+    """
+
+@app.route("/automatizacion")
+def automatizacion():
+    return """
+    <h1 style='color:white;
+    font-family:Arial;
+    text-align:center;
+    margin-top:100px;
+    background:black;'>
+    Página de procesos próximamente
+    </h1>
+    """
+
+@app.route("/estadisticas")
+def estadisticas():
+    return """
+    <h1 style='color:white;
+    font-family:Arial;
+    text-align:center;
+    margin-top:100px;
+    background:black;'>
+    Página de estadísticas próximamente
+    </h1>
+    """
 
 if __name__ == "__main__":
     app.run(debug=True)
